7f08bf07d9d8dec2627de21fb249155956759f05
Ninad Khargonkar <ninad.khargonkar@gmail.com>
2025-02-26 16:32:52 -0600

    some base level constants

---
+++ scene_setup/scripts/utils/constants.py
@@ -0,0 +1,40 @@
+OBJID_TO_SCENES = {
+    "003" : ["s27", "s48"],
+    "004" : ["s10", "s141"],
+    "005" : ["s65", "s130"],
+    "006" : ["s83", "s130"],
+    "007" : ["s56", "s122"],
+    "008" : ["s38", "s56"],
+    "009" : ["s122", "s39"],
+    "010" : ["s77", "s83"],
+    "011" : ["s10", "s27"],
+    "021" : ["s83", "s104"],
+    "024" : ["s77", "s141"],
+    "025" : ["s27", "s84"],
+    "035" : ["s10", "s68"],
+    "037" : ["s148", "s161"],
+    "040" : ["s148", "s161"],
+    "052" : ["s27", "s48"],
+}
+
+YCBID_MAP = {
+    "003" : "003_cracker_box",
+    "004" : "004_sugar_box",
+    "005" : "005_tomato_soup_can",
+    "006" : "006_mustard_bottle",
+    "007" : "007_tuna_fish_can",
+    "008" : "008_pudding_box",
+    "009" : "009_gelatin_box",
+    "010" : "010_potted_meat_can",
+    "011" : "011_banana",
+    "021" : "021_bleach_cleanser",
+    "024" : "024_bowl",
+    "025" : "025_mug",
+    "035" : "035_power_drill",
+    "037" : "037_scissors",
+    "040" : "040_large_marker",
+    "052" : "052_extra_large_clamp",
+}
+
+VALID_POSE_METHODS = {"gazebo", "poserbpf", "posecnn", "isaac", "gdrnpp"}
+
